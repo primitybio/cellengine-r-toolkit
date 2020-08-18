ae58f2be040a755b922611c1ca7e33322a044a24
Zach Bjornson <zbbjornson@gmail.com>
2020-08-18 13:56:12 -0700

    Rebuild documentation

--- R/getExperiment.R
+++ R/getExperiment.R
@@ -7,7 +7,10 @@
 #' @export
 #' @examples
 #' \dontrun{
+#' # Retrieve by ID
 #' getExperiment(experimentId)
+#'
+#' # Lookup by name
 #' getExperiment(byName("my experiment"))
 #' }
 getExperiment = function(experimentId, params = list()) {
