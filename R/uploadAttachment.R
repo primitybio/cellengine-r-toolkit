c8696e226a15d68a22be8745b1d1897b7bb6baae
gegnew <g.egnew@gmail.com>
2021-02-17 19:43:09 +0100

    Add uploadAttachment function
    Update uploadAttachment test
    
    Add uploadAttachment to pkgdown

---
+++ R/uploadAttachment.R
@@ -0,0 +1,19 @@
+#' Upload attachment
+#'
+#' Uploads an attachment to an experiment.
+#'
+#' @param experimentId ID of experiment to which to upload.
+#' @param attachmentPath Path to attachment.
+#' @export
+#' @examples
+#' \dontrun{
+#' uploadFcsFile(experimentId, "/path/to/file")
+#' }
+uploadAttachment = function(experimentId, attachmentPath) {
+  checkDefined(experimentId)
+  experimentId = lookupByName("experiments", experimentId)
+  body = list("file" = httr::upload_file(attachmentPath))
+  ensureBaseUrl()
+  fullURL = paste(pkg.env$baseURL, "experiments", experimentId, "attachments", sep = "/")
+  handleResponse(httr::POST(fullURL, body = body, httr::user_agent(ua)))
+}
